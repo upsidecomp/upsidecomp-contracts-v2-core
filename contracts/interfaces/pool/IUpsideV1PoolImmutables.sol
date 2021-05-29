6cdf75137e76130d00d57216e826db2f1d0a7b11
Jeevan Pillay <jeevan.pillay8@gmail.com>
2021-05-29 20:37:19 +1000

    refixed error with IUpsideV1PoolImmutables wrong name

--- contracts/interfaces/pool/IUpsideV1PoolImmutables.sol
+++ contracts/interfaces/pool/IUpsideV1PoolImmutables.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Pool state that never changes
 /// @notice These parameters are fixed for a pool forever, i.e., the methods will always return the same values
-interface IUniswapV3PoolImmutables {
+interface IUpsideV1PoolImmutables {
     /// @notice The contract that deployed the pool, which must adhere to the IUniswapV3Factory interface
     /// @return The contract address
     function factory() external view returns (address);
